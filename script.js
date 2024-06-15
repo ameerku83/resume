4d3ad56bafd3a9ea5b9424edd24dab1edc5c9494
HAMEER KU <ameerku83@gmail.com>
2024-06-15 20:41:11 +0530

    new

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
     
 
 
+const password = document.getElementById("password")
+const Cpassword=document.getElementById("Cpassword")
 
 
     function handleSubmit(){
       const message=document.getElementById("message")
-      const name = document.getElementById("name").value
-      const email = document.getElementById("email").value
-      const password = document.getElementById("password").value
-      const Phone = document.getElementById("Phone").value
-      const address=document.getElementById("address").value
-      const namecorect=document.getElementById("namecorect")
-      const emailCorect=document.getElementById("emailcorect")
-      const passwordCorrect=document.getElementById("passwordcorrect")
-      const phonCcorrect=document.getElementById("phonecorrect")
-      const addressCorect=document.getElementById("addresscorect")
+const name = document.getElementById("name").value
+const email = document.getElementById("email").value
+
+const namecorect=document.getElementById("namecorect")
+const emailCorect=document.getElementById("emailcorect")
+const passwordCorrect=document.getElementById("passwordcorrect")
+  const  cpasswordcorrect=document.getElementById("cpasswordcorrect")
+      
         namecorect.style.color="green"
        message.style.display="none"
-       if(name.length<2){
+       if( !name.trim() ){
            message.innerText="please enter name...!"
            message.style.display="block"
             namecorect.innerText=" "
@@ -28,7 +28,7 @@
          
            
        }
-       if( /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/.test(email)==false){
+       if( !/^[a-z0-9]+@[a-z]+\.[a-z]{3}$/.test(email)){
            message.innerText="entera valid email. use small letters..!"
            message.style.display="block"
            emailCorect.innerText=""
@@ -39,8 +39,8 @@
            
            
        }
-       if(!/^(?=.*[a-z])(?=.*[A-Z]).{6,15}$/.test(password)){
-           message.innerText="password must be greater than 5 one uppercase...!"
+       if(!/^(?=.*[a-z])(?=.*[A-Z]).{5,15}$/.test(password.value) ){
+           message.innerText="password must be greater than 4 one uppercase...!"
            message.style.display="block"
            passwordCorrect.innerText=""
            return false
@@ -50,30 +50,59 @@
            
            
        }
-
-       if(Phone.length<7){
-         message.innerText="enter your phone number...!"
-         message.style.display="block"
-          phonCcorrect.innerText=""
-         return false
-     }else{
-      phonCcorrect.style.color="green"
-      phonCcorrect.innerText="✓..!"
-      
+       if(password.value !== Cpassword.value){
+        message.innerText="Enter same password"
+        message.style.display="block"
+        passwordCorrect.innerText=""
+        return false
+    }else{
+      passwordCorrect.style.color="green"
+      cpasswordcorrect.innerText="✓..!"
+        
         
     }
-     if(address.length<10){
-      message.innerText="enter message ,at least 10 character...!"
-      message.style.display="block"
-      addressCorect.innerText=""
-      return false
-  }else{
-   addressCorect.style.color="green"
-   addressCorect.innerText="✓..!"
-     
-     
- }
 
-               alert("successfully saved")
+
+         message.style.display="block"
+          message.style.color="green"
+          message.innerText="success please Login..!"
+                alert(" Signup success please login")
                return true
+               
            }
+
+            function handleLogin(){
+
+            const logMessage=document.getElementById("logMessage")
+      const logEmail = document.getElementById("logEmail").value
+   
+      const logPassword = document.getElementById("logPassword").value
+      if( !/^[a-z0-9]+@[a-z]+\.[a-z]{3,3}$/.test(logEmail)){
+           logMessage.innerText="please enter a valid email ...!"
+           logMessage.style.display="block"
+           return false
+       }
+          const regx=  /^(?=.*[a-z])(?=.*[A-Z]).{5,15}$/
+            
+            
+       if(!regx.test(logPassword) ){
+           logMessage.innerText= "password must be greater than 4 one uppercase...!"
+          
+           logMessage.style.display="block"
+           return false
+       }
+       
+       alert("Login success ")
+       return true
+
+        }
+          
+        function viewPassword(){
+          if(password.type=="password" && Cpassword.type=="password"){
+            password.type="text" 
+             Cpassword.type="text"
+          }else{
+             password.type="password" 
+             Cpassword.type="password"
+          }
+        }
